93271b4563a64c0d10c0b8d9c30e821070a2cdcb
gdiaz <gdiaz@snapfinance.com>
2022-02-23 08:47:42 -0600

    Eclipse Format

--- src/main/java/io/github/gdiazs/demo/DemoWebApplicationInitializer.java
+++ src/main/java/io/github/gdiazs/demo/DemoWebApplicationInitializer.java
@@ -1,31 +1,32 @@
 package io.github.gdiazs.demo;
 
-import io.github.gdiazs.demo.config.WebConfig;
 import javax.servlet.Filter;
+
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class DemoWebApplicationInitializer
-    extends AbstractAnnotationConfigDispatcherServletInitializer {
+import io.github.gdiazs.demo.config.WebConfig;
 
-  @Override
-  protected Class<?>[] getRootConfigClasses() {
-    return null;
-  }
+public class DemoWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-  @Override
-  protected Class<?>[] getServletConfigClasses() {
-    return new Class<?>[] {WebConfig.class};
-  }
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
 
-  @Override
-  protected String[] getServletMappings() {
-    return new String[] {"/app/*"};
-  }
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
 
-  @Override
-  protected Filter[] getServletFilters() {
-    var securityFilterChain = new DelegatingFilterProxy("springSecurityFilterChain");
-    return new Filter[] {securityFilterChain};
-  }
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/app/*" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		var securityFilterChain = new DelegatingFilterProxy("springSecurityFilterChain");
+		return new Filter[] { securityFilterChain };
+	}
 }
